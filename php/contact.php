69d0c2f6b263bc9d690c1a08b1324bf91a05b19c
Gabriel <gabrielifoga@yahoo.com>
2024-06-10 05:39:39 +0100

    Rebrand resume

--- php/contact.php
+++ php/contact.php
@@ -3,7 +3,7 @@
 header('Content-Type: text/html; charset=utf-8');
 
 // Email recipient
-$EmailTo = "YOUR_EMAIL@EXAMPLE.COM";
+$EmailTo = "gabrielifoga@yahoo.com";
 
 $errors = "";
 
@@ -37,6 +37,13 @@ if (empty($_POST["message"])) {
 
 // Prepare email body text
 $Body = "";
+$Body .= "___________________________";
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $Subject;
+$Body .= "\n";
+$Body .= "---------------------------";
+$Body .= "\n";
 $Body .= "Name: ";
 $Body .= $name;
 $Body .= "\n";
@@ -46,19 +53,38 @@ $Body .= "\n";
 $Body .= "Message: ";
 $Body .= $message;
 $Body .= "\n";
+$Body .= "___________________________";
+echo "Request data" . $Body;
+// Open file in append mode
+$file = fopen("contacts.txt", "a");
 
-$Headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
-$Headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
-$Headers .= "From:".$email . "\r\n";
+// Write form data to the file
+fwrite($file, "$Body\n");
 
-// Send email
-$success = mail($EmailTo, '=?UTF-8?B?' . base64_encode($Subject) . '?=' , base64_encode($Body), $Headers);
+// Close the file
+fclose($file);
 
-// Redirect to success page
-if ($success && $errors == ""){
-   echo 'success';
-}
-else{
+if(empty($errors)) 
+{
+    echo 'success';
+} else  {
     echo $errors;
 }
+
+
+
+// $Headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
+// $Headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
+// $Headers .= "From:".$email . "\r\n";
+
+// Send email
+// $success = mail($EmailTo, '=?UTF-8?B?' . base64_encode($Subject) . '?=' , base64_encode($Body), $Headers);
+
+// // Redirect to success page
+// if ($success && $errors == ""){
+//    echo 'success';
+// }
+// else{
+//     echo $errors;
+// }
 ?>
